a4aea9fb68a44f3208194f56abcdbfb224288003
Matthew Bernhardt <mjbernha@mit.edu>
2015-11-14 12:13:44 -0500

    Adds boilerplate php file

---
+++ posttype.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Plugin Name: Plugin Template
+ * Plugin URI: https://github.com/matt-bernhardt/wp-plugin-template
+ * Description: This is a sample plugin
+ * Version: 0.1
+ * Author: Matt Bernhardt
+ * Author URI: https://github.com/matt-bernhardt
+ * License: GPL2
+ */
+
+/**
+ * {Plugin Name} is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * any later version.
+ *  
+ * {Plugin Name} is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *  
+ * You should have received a copy of the GNU General Public License
+ * along with {Plugin Name}. If not, see {URI to Plugin License}.
+ */
+?>
